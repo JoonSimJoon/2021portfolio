2dcb8616a8409b17e15a5609799aa799005c5a2b
SimJoon <wesley0940@gmail.com>
2021-07-30 01:09:47 +0900

    20210730

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button, ButtonToolbar } from 'react-bootstrap';
-import ReactFullpage from '@fullpage/react-fullpage';
 
-function App() {
+const App = ()=> {
+  const style = {
+    color : 'white'
+  };
   return (
-    <ReactFullpage
-
-    //fullpage options
-    licenseKey = {'YOUR_KEY_HERE'}
-    scrollingSpeed = {1000} /* Options here */
-    scrollHorizontally = {true}  /* Because we are using the extension */
-    scrollHorizontallyKey = {'YOUR KEY HERE'}
-
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
+    <div style={style}>hi</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
