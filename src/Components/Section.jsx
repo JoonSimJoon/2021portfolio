6964bad24200aef659bc4404e5cb8d5bf7664eb7
SimJoon <wesley0940@gmail.com>
2021-09-01 01:05:41 +0900

    20210901

--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -11,39 +11,37 @@ height: 58.6%;
 bottom: 34%;
 right: 0.3%;
 position:fixed; 
-box-sizing: border-box;
 overflow: auto;
-display : inline-block;
+flex-direction: row;
 background-color:#252526;
 border: 1px solid #3f3f46;
 color:#fff;
 `;
 
 const Left_box = styled.div`
-height: 40%;
-width: 1%;
+flex : 1;
+height: auto;
+float: left;
+display: inline-block;
 background-color:#333333;
-position: fixed;
 border: 1px solid #3f3f46;
-display : inline-block;
 `;
 
 const Text_box = styled.div`
 color:#fff;
-display : inline-block;
-position:static;
+float: right;
+height: 3px;
+display: inline-block;
+flex: 1;
 `;
 
 
 const Section = props => {  
     return (
      <>
-     
-
         <Wrapper>
         <Left_box></Left_box>
-        <Text_box>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias aspernatur, assumenda perferendis laudantium distinctio nihil voluptatem rerum deleniti corrupti consequuntur earum quod numquam! 
-            Autem, esse cumque? Laboriosam consequuntur veritatis asperiores!</Text_box>
+        <Text_box>왜안되는데</Text_box>
         
         </Wrapper>      
      </>
